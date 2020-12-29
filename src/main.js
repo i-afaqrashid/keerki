6409fa32017bc37dd666e8a56114aefbf77170ee
i-afaqrashid <i.afaqrashid@gmail.com>
2020-12-29 18:26:56 +0000

    new plugins

--- src/main.js
+++ src/main.js
@@ -5,11 +5,15 @@ import './plugins/bootstrap-vue'
 import App from './App.vue'
 import i18n from './i18n'
 import vuetify from './plugins/vuetify';
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
   i18n,
   vuetify,
+  router,
+  store,
   render: h => h(App)
 }).$mount('#app')
